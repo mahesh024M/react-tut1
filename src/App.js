ddf1656aafb1de55dce9cb648076ac390b5170b9
Mahesh <maheshmaraju24@gmail.com>
2023-04-12 10:49:58 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./styles.css";
+import Test1 from "./components/Test1";
 
 export default function App() {
+
+  let [userName,setUserName]=useState('Ramana');
+
+  function changeUserName(){
+    setUserName('vedant');
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App" >
+          <h1>React Hierarchy</h1>
+           <Test1/>
     </div>
   );
 }
---
+++ src/components/Test1.js
@@ -0,0 +1,12 @@
+
+
+export default function Test1(props){
+
+    console.log(props);
+
+    return(
+      <div>
+       
+      </div>
+    );
+}
