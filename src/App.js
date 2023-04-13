9dfbe64088a36ded46317edba18538868f5597b9
Mahesh <maheshmaraju24@gmail.com>
2023-04-13 13:00:08 +0000

    resuability of components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import { useState } from "react";
 import "./styles.css";
 import Test1 from "./components/Test1";
+import Userdetails from "./components/Userdetails";
 
 export default function App() {
 
   let [userName,setUserName]=useState('Ramana');
-
+  let [city,setCity]=useState('Hyderabad');
   function changeUserName(){
     setUserName('vedant');
   }
 
+  // function to bring data from child test1
+  const getdata=(data)=>{
+         console.log("Data receieved from child is: ",data);
+  }
+
+  const changecity=()=>{
+     setCity('Karimnagar');
+  }
+
+  const  usersArray=[
+     {username:'Raju',
+     salary:20000,
+     age:25
+     },
+
+     {username:'Ramesh',
+     salary:30000,
+     age:27
+     },
+     {username:'Ravi',
+     salary:40000,
+     age:30
+     }
+
+  ]
+
+  let [users,setusers]=useState(usersArray);
+
   return (
     <div className="App" >
           <h1>React Hierarchy</h1>
-           <Test1 userName={userName}/>
+          <h2>{city}</h2>
+       <Test1 userName={userName} getdata={getdata} city={city} />
+
+       {
+          users.map((userobj)=> <Userdetails  user={userobj}  />)
+       }
+          
+          
+        
     </div>
   );
 }
--- src/components/Test1.js
+++ src/components/Test1.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 
 
 export default function Test1(props){
 
     console.log("data is ",props);
-      
+   
+    let[age,setage]=useState(20);
+
     return(
       <div>
        <h1>Test1 component</h1>
         <h1>Data received from parent is:{props.userName}</h1>
+        <button onClick={()=>props.getdata(age)}>submit</button>
       </div>
     );
 }
